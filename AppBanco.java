c49037dc73c6a808207c36e87f1ce772e2136f9a
biel0209 <52585328+biel0209@users.noreply.github.com>
2021-01-05 17:39:33 -0300

    Update AppBanco.java

--- AppBanco.java
+++ AppBanco.java
@@ -1,4 +1,3 @@
-import Conta;
 public class AppBanco{
     public static void main(String[] args){
         Conta conta1 = new Conta ("Jose","03549135177","joseberson45@gmail.com","998546621","108",
@@ -7,16 +6,35 @@ public class AppBanco{
                                    "1733","5584917","013",5000f);
         Conta conta3 = new Conta ("Andre","54691532105","andrelg@gmail.com","999546278","108",
                                    "1733","2465879","013",2300f);     
-                                   
-                                   
-        System.out.println("Saldo da conta 1: " + conta1.getSaldo());
-        System.out.println("Saldo da conta 3: " + conta3.getSaldo());
 
-        System.out.println("Transferindo...");
+        inicializarExtrato(conta1);
+        inicializarExtrato(conta2);
+        inicializarExtrato(conta3);
+
+
         conta1.transferir(conta1, conta3, 150f);
 
-        System.out.println("Saldo da conta 1: " + conta1.getSaldo());
-        System.out.println("Saldo da conta 3: " + conta3.getSaldo());
+        conta2.transferir(conta2, conta1, 500f);
+
+        conta1.transferir(conta1, conta2, 2500f);
+
+        conta1.exibirSaldo();
+        conta2.exibirSaldo();
+        conta3.exibirSaldo();
+
+        conta3.transferir(conta3, conta2, 1500f);
+
+        conta3.transferir(conta3, conta1, 1280f);
+
+
+        conta1.exibirExtrato();
+        conta2.exibirExtrato(); 
+        conta3.exibirExtrato();
 
     }
+
+    public static void inicializarExtrato(Conta conta){
+        conta.setExtrato("Saldo: " + conta.getSaldo());
+    }
+
 }
